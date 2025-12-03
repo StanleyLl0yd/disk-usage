5ce9d6658708201a48afd381735ce02e2afecac7
“Stanley <stanleylloyd@gmail.com>
2025-12-03 21:27:16 +0300

    Initial Commit

---
+++ DiskUsage/DiskUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskUsageApp.swift
+//  DiskUsage
+//
+//  Created by Сергей Лесневский on 03.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DiskUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
